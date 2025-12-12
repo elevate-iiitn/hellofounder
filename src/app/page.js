f6621c97228a75d2a0be7dcf0c0d9740e8a188a7
Arnab Mistry <88276146+ArnabMistry@users.noreply.github.com>
2025-12-12 10:42:26 +0530

    Added Navbar

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,12 @@
 import DarkVeil from '../components/darkveil';
-
+import { Header } from '../components/header-2';
 
 export default function Home() {
   return (
-    <>
+  <>
     <div className="absolute inset-0 z-0 pointer-events-none">
             <DarkVeil />
-          </div>
-          </>
+    </div>
+    <Header />
+  </>
   );}
