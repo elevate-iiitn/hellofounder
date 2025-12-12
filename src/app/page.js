4be6a9dda4f60c660564ae13c69cb69f2b2b1029
Arnab Mistry <88276146+ArnabMistry@users.noreply.github.com>
2025-12-12 12:25:00 +0530

    Added Section Outline

--- src/app/page.js
+++ src/app/page.js
@@ -9,5 +9,33 @@ export default function Home() {
     </div>
     <Header />
     <Hero />
+
+    <h2 className="mb-5 text-center font-medium text-white text-xl tracking-tight md:text-3xl">
+          <span className="text-white">Founding Cohort starts{' '}</span>
+          <span className="font-semibold text-teal-400">February 15, 2026</span>
+    </h2>
+        <br/><br/><br/><br/><br/><br/><br/>
+
+    <h2 className="mb-5 text-center font-medium text-white text-xl tracking-tight md:text-3xl">
+          <span className="text-white">Learn all the relevant skills to be </span>
+          <br/>
+          <span className="font-semibold text-teal-400">Investment Ready</span>
+    </h2>
+        <br/><br/><br/><br/><br/><br/><br/>
+    <h2 className="mb-5 text-center font-medium text-white text-xl tracking-tight md:text-3xl">
+          <span className="text-white">Meet Our{' '}</span>
+          <span className="font-semibold text-teal-400">Mentors</span>
+    </h2>
+        <br/><br/><br/><br/><br/><br/><br/>
+    <h2 className="mb-5 text-center font-medium text-white text-xl tracking-tight md:text-3xl">
+          <span className="text-white">Inside the{' '}</span>
+          <span className="font-semibold text-teal-400">HelloFounder Program</span>
+    </h2>
+        <br/><br/><br/><br/><br/><br/><br/>
+    <h2 className="mb-5 text-center font-medium text-white text-xl tracking-tight md:text-3xl">
+          <span className="text-white">Curriculum Overview</span>
+          <br/>
+          <span className="font-semibold text-teal-400">Designed for Emerging Founders</span>
+    </h2>
   </>
   );}
