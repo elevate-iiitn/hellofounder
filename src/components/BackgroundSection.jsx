92739ada8f45e0ede57f1aa8155d8f277cd9ea70
Arnab Mistry <88276146+ArnabMistry@users.noreply.github.com>
2025-12-22 15:34:38 +0530

    (UI) Fixed bg contrast and applied Visual Fadeout

--- src/components/BackgroundSection.jsx
+++ src/components/BackgroundSection.jsx
@@ -3,8 +3,8 @@ export default function BackgroundSection({ children, className = "" }) {
     <section
       className={`relative bg-[url('/images/isfbg.png')] bg-cover bg-center bg-no-repeat ${className}`}
     >
-      {/* optional overlay for contrast */}
-      <div className="absolute inset-0 bg-black/50" />
+      {/* bottom fade to black */}
+      <div className="pointer-events-none absolute inset-x-0 bottom-0 h-28 bg-linear-to-b from-transparent to-black" />
 
       {/* content */}
       <div className="relative z-10">{children}</div>
